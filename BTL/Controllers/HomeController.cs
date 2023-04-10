862d72ace4d37a19e62892e8538d30d4e12bd818
anhdai12 <nguyendai180802@gmail.com>
2023-04-10 23:10:57 +0700

    Home phong chi tiet phong

--- BTL/Controllers/HomeController.cs
+++ BTL/Controllers/HomeController.cs
@@ -6,16 +6,16 @@ using X.PagedList;
 
 namespace BTL.Controllers
 {
-	public class HomeController : Controller
-	{
+    public class HomeController : Controller
+    {
         QlkhachSanAspContext db = new QlkhachSanAspContext();
 
         private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
         public IActionResult Index(int? page)
         {
@@ -28,47 +28,21 @@ namespace BTL.Controllers
             return View(lst);
         }
 
-        public IActionResult Room(int? page)
-        {
-            int pageSize = 3;
-            int pageNumber = page == null || page < 0 ? 1 : page.Value;
-
-            //var room = db.LoaiPhongs.Include("TenPhong").Include("TinhTrang").ToList();
-            var lstLoaiPhong = db.LoaiPhongs.AsNoTracking().OrderBy(x => x.MaLp);
-
-            PagedList<LoaiPhong> lst = new PagedList<LoaiPhong>(lstLoaiPhong, pageNumber, pageSize);
-            return View(lst);
-        }
-
-        public IActionResult PhongTheoLoai(string maLoai, int? page)
-        {
-            int pageSize = 8;
-            int pageNumber = page == null || page < 0 ? 1 : page.Value;
-            var lstphong = db.LoaiPhongs.AsNoTracking().Where(x => x.MaLp == maLoai).OrderBy(x => x.Gia);
-            PagedList<LoaiPhong> lst = new PagedList<LoaiPhong>(lstphong, pageNumber, pageSize);
-            ViewBag.maloai = maLoai;
-            return View(lst);
-        }
-
-        public IActionResult ChiTietPhong(string maloai)
-        {
-            //var room = db.LoaiPhongs.Include("TenPhong").Include("TinhTrang").ToList();
-
-            var room = db.LoaiPhongs.SingleOrDefault(x => x.MaLp == maloai);
-            var anhPhong = db.LoaiPhongs.Where(x => x.MaLp == maloai).ToList();
-            ViewBag.anhPhong = anhPhong;
-            return View(room);
-        }
-
-        public IActionResult Privacy()
+		public IActionResult CheckRoom()
 		{
-			return View();
+			var lstLoaiPhong = db.LoaiPhongs.AsNoTracking().OrderBy(x => x.Gia);
+			return View(lstLoaiPhong);
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+		public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
---
+++ BTL/Controllers/RoomController.cs
@@ -0,0 +1,62 @@
+﻿using BTL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using X.PagedList;
+
+namespace BTL.Controllers
+{
+    public class RoomController : Controller
+    {
+
+		QlkhachSanAspContext db = new QlkhachSanAspContext();
+
+
+		public IActionResult Index(int? page)
+		{
+			int pageSize = 3;
+			int pageNumber = page == null || page < 0 ? 1 : page.Value;
+
+			var lstLoaiPhong = db.LoaiPhongs.AsNoTracking().OrderBy(x => x.MaLp);
+
+			PagedList<LoaiPhong> lst = new PagedList<LoaiPhong>(lstLoaiPhong, pageNumber, pageSize);
+			return View(lst);
+		}
+
+        public IActionResult PhongTheoLoai(string maLoai, int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+
+			var phong = db.LoaiPhongs.Where(x => x.MaLp == maLoai).FirstOrDefault();
+            ViewBag.Gia = phong.Gia;
+
+            var lstPhong = db.Phongs.AsNoTracking().Where(x => x.MaLp == maLoai).OrderBy(x => x.MaLp);
+            PagedList<Phong> lst = new PagedList<Phong>(lstPhong, pageNumber, pageSize);
+            ViewBag.maloai = maLoai;
+            return View(lst);
+        }
+
+        public IActionResult ChiTietPhong(string maphong, string maloai)
+		{
+			var Lphong = db.LoaiPhongs.SingleOrDefault(x => x.MaLp == maloai);
+
+			var phong = db.Phongs.Where(x => x.MaPhong == maphong && x.MaLp == maloai).FirstOrDefault();
+
+			ViewBag.TenPhong = phong.TenPhong;
+
+			ViewBag.TinhTrang = phong.TinhTrang;
+
+			var ctAnh = db.Ctanhs.Where(x => x.MaLp == maloai).ToList();
+			ViewBag.ctAnh = ctAnh;
+
+			return View(Lphong);
+		}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
