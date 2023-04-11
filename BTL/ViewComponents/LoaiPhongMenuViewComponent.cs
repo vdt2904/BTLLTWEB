a9ad526cddf29978633fcc3cc264cf8d8ca4e46d
vdt2904 <vudoanhthai12b2002hla@gmail.com>
2023-04-11 10:26:03 +0700

    Revert "Home phong chi tiet phong"
    This reverts commit 862d72ace4d37a19e62892e8538d30d4e12bd818.

---
+++ BTL/ViewComponents/LoaiPhongMenuViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using BTL.Models;
+using Microsoft.AspNetCore.Mvc;
+using BTL.Repository;
+namespace BTL.ViewComponents
+{
+	public class LoaiPhongMenuViewComponent : ViewComponent
+	{
+		private readonly ILoaiPhongRepository _loaiPhongRepository;
+		public LoaiPhongMenuViewComponent(ILoaiPhongRepository loaiPhongRepository)
+		{
+			_loaiPhongRepository = loaiPhongRepository;	
+		}
+		public IViewComponentResult Invoke()
+		{
+			var loaiPhong = _loaiPhongRepository.GetAll().OrderBy(x => x.LoaiPhong1);
+			return View(loaiPhong);
+		}
+	}
+}
