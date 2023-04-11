8ba97eaf83e50bebd45525be24de18f72f5a8cb1
vdt2904 <vudoanhthai12b2002hla@gmail.com>
2023-04-11 20:39:09 +0700

    fix bug

--- BTL/Controllers/HomeController.cs
+++ BTL/Controllers/HomeController.cs
@@ -69,8 +69,6 @@ namespace BTL.Controllers
             int pageSize = 2;
             int pageNumber = page == null || page < 0 ? 1 : page.Value;
             var lstBlog = db.Blogs.AsNoTracking().OrderBy(x => x.NgayDang);
-
-<<<<<<< HEAD
             PagedList<Blog> lst = new PagedList<Blog>(lstBlog, pageNumber, pageSize);
             return View(lst);
 
@@ -95,23 +93,10 @@ namespace BTL.Controllers
         {
             return View();
         }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-=======
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
->>>>>>> a9ad526cddf29978633fcc3cc264cf8d8ca4e46d
 }
